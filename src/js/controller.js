0a67fa58bedc6418394103777f059ebe400ec16f
Anonav0 <swarnavokhanra@gmail.com>
2024-10-24 18:22:55 +0530

    v1:load recipes from the API

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    // console.log(res);
+    const data = await res.json();
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      title: recipe.title,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipe();
