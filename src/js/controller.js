5a50a16cd6f886142f07b582b5323686923528c5
Anonav0 <swarnavokhanra@gmail.com>
2024-10-24 19:15:32 +0530

    v1:render 1 recipe: ingredients render

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 import icons from 'url:../img/icons.svg';
 
+import { Fraction } from 'fractional';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -52,14 +54,18 @@ const showRecipe = async function () {
         <svg class="recipe__info-icon">
           <use href="${icons}#icon-clock"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+        <span class="recipe__info-data recipe__info-data--minutes">${
+          recipe.cookingTime
+        }</span>
         <span class="recipe__info-text">minutes</span>
       </div>
       <div class="recipe__info">
         <svg class="recipe__info-icon">
           <use href="${icons}#icon-users"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+        <span class="recipe__info-data recipe__info-data--people">${
+          recipe.servings
+        }</span>
         <span class="recipe__info-text">servings</span>
 
         <div class="recipe__info-buttons">
@@ -91,27 +97,26 @@ const showRecipe = async function () {
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
+      ${recipe.ingredients
+        .map(
+          ing => `
         <li class="recipe__ingredient">
           <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
           </svg>
-          <div class="recipe__quantity">1000</div>
+          <div class="recipe__quantity">${
+            ing.quantity ? new Fraction(ing.quantity).toString() : ''
+          }</div>
           <div class="recipe__description">
-            <span class="recipe__unit">g</span>
-            pasta
+            <span class="recipe__unit">${ing.unit}</span>
+            ${ing.description}
           </div>
         </li>
 
-        <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="${icons}#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">0.5</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">cup</span>
-            ricotta cheese
-          </div>
-        </li>
+        `
+        )
+        .join('')}
+
       </ul>
     </div>
 
@@ -119,7 +124,9 @@ const showRecipe = async function () {
       <h2 class="heading--2">How to cook it</h2>
       <p class="recipe__directions-text">
         This recipe was carefully designed and tested by
-        <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+        <span class="recipe__publisher">${
+          recipe.publisher
+        }</span>. Please check out
         directions at their website.
       </p>
       <a
