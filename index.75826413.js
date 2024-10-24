98edd32cf012d1080114bdf82640e447606c2218
Anonav0 <swarnavokhanra@gmail.com>
2024-10-24 20:38:11 +0530

    update production build v1.0 : bug fix

--- index.75826413.js
+++ index.75826413.js
@@ -1,35 +1,35 @@
-function e(e){return e&&e.__esModule?e.default:e}var t,n,r,i=globalThis,o={},s={},a=i.parcelRequire6e42;null==a&&((a=function(e){if(e in o)return o[e].exports;if(e in s){var t=s[e];delete s[e];var n={id:e,exports:{}};return o[e]=n,t.call(n.exports,n,n.exports),n.exports}var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}).register=function(e,t){s[e]=t},i.parcelRequire6e42=a),(0,a.register)("27Lyk",function(e,t){Object.defineProperty(e.exports,"register",{get:()=>n,set:e=>n=e,enumerable:!0,configurable:!0});var n,r=new Map;n=function(e,t){for(var n=0;n<t.length-1;n+=2)r.set(t[n],{baseUrl:e,path:t[n+1]})}}),a("27Lyk").register(new URL("",import.meta.url).toString(),JSON.parse('["f9fpV","index.75826413.js","eyyUD","icons.0dd9027a.svg"]'));var c={};c=new URL("icons.0dd9027a.svg",import.meta.url).toString(),(Fraction=function(e,t){if(void 0!==e&&t)"number"==typeof e&&"number"==typeof t?(this.numerator=e,this.denominator=t):"string"==typeof e&&"string"==typeof t&&(this.numerator=parseInt(e),this.denominator=parseInt(t));else if(void 0===t){if("number"==typeof(num=e))this.numerator=num,this.denominator=1;else if("string"==typeof num){var n,r,i=num.split(" ");if(i[0]&&(n=i[0]),i[1]&&(r=i[1]),n%1==0&&r&&r.match("/"))return new Fraction(n).add(new Fraction(r));if(!n||r)return;if("string"==typeof n&&n.match("/")){var o=n.split("/");this.numerator=o[0],this.denominator=o[1]}else{if("string"==typeof n&&n.match("."))return new Fraction(parseFloat(n));this.numerator=parseInt(n),this.denominator=1}}}this.normalize()}).prototype.clone=function(){return new Fraction(this.numerator,this.denominator)},Fraction.prototype.toString=function(){if("NaN"===this.denominator)return"NaN";var e=this.numerator/this.denominator>0?Math.floor(this.numerator/this.denominator):Math.ceil(this.numerator/this.denominator),t=this.numerator%this.denominator,n=this.denominator,r=[];return 0!=e&&r.push(e),0!=t&&r.push((0===e?t:Math.abs(t))+"/"+n),r.length>0?r.join(" "):0},Fraction.prototype.rescale=function(e){return this.numerator*=e,this.denominator*=e,this},Fraction.prototype.add=function(e){var t=this.clone();return e instanceof Fraction?e=e.clone():e=new Fraction(e),td=t.denominator,t.rescale(e.denominator),e.rescale(td),t.numerator+=e.numerator,t.normalize()},Fraction.prototype.subtract=function(e){var t=this.clone();return e instanceof Fraction?e=e.clone():e=new Fraction(e),td=t.denominator,t.rescale(e.denominator),e.rescale(td),t.numerator-=e.numerator,t.normalize()},Fraction.prototype.multiply=function(e){var t=this.clone();if(e instanceof Fraction)t.numerator*=e.numerator,t.denominator*=e.denominator;else{if("number"!=typeof e)return t.multiply(new Fraction(e));t.numerator*=e}return t.normalize()},Fraction.prototype.divide=function(e){var t=this.clone();if(e instanceof Fraction)t.numerator*=e.denominator,t.denominator*=e.numerator;else{if("number"!=typeof e)return t.divide(new Fraction(e));t.denominator*=e}return t.normalize()},Fraction.prototype.equals=function(e){e instanceof Fraction||(e=new Fraction(e));var t=this.clone().normalize(),e=e.clone().normalize();return t.numerator===e.numerator&&t.denominator===e.denominator},Fraction.prototype.normalize=(t=function(e){return"number"==typeof e&&(e>0&&e%1>0&&e%1<1||e<0&&e%-1<0&&e%-1>-1)},n=function(e,t){if(!t)return Math.round(e);var n=Math.pow(10,t);return Math.round(e*n)/n},function(){if(t(this.denominator)){var e=n(this.denominator,9),r=Math.pow(10,e.toString().split(".")[1].length);this.denominator=Math.round(this.denominator*r),this.numerator*=r}if(t(this.numerator)){var e=n(this.numerator,9),r=Math.pow(10,e.toString().split(".")[1].length);this.numerator=Math.round(this.numerator*r),this.denominator*=r}var i=Fraction.gcf(this.numerator,this.denominator);return this.numerator/=i,this.denominator/=i,(this.numerator<0&&this.denominator<0||this.numerator>0&&this.denominator<0)&&(this.numerator*=-1,this.denominator*=-1),this}),Fraction.gcf=function(e,t){var n=[],r=Fraction.primeFactors(e),i=Fraction.primeFactors(t);return(r.forEach(function(e){var t=i.indexOf(e);t>=0&&(n.push(e),i.splice(t,1))}),0===n.length)?1:function(){var e,t=n[0];for(e=1;e<n.length;e++)t*=n[e];return t}()},Fraction.primeFactors=function(e){for(var t=Math.abs(e),n=[],r=2;r*r<=t;)t%r==0?(n.push(r),t/=r):r++;return 1!=t&&n.push(t),n},r=Fraction;const u=document.querySelector(".recipe");!async function(){try{let t=await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"),{recipe:n}=(await t.json()).data;n={id:n.id,image:n.image_url,sourceUrl:n.source_url,ingredients:n.ingredients,publisher:n.publisher,title:n.title,servings:n.servings,cookingTime:n.cooking_time,ingredients:n.ingredients},console.log(n);let i=`
+function e(e){return e&&e.__esModule?e.default:e}var i=globalThis,s={},t={},n=i.parcelRequire6e42;null==n&&((n=function(e){if(e in s)return s[e].exports;if(e in t){var i=t[e];delete t[e];var n={id:e,exports:{}};return s[e]=n,i.call(n.exports,n,n.exports),n.exports}var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}).register=function(e,i){t[e]=i},i.parcelRequire6e42=n),(0,n.register)("27Lyk",function(e,i){Object.defineProperty(e.exports,"register",{get:()=>s,set:e=>s=e,enumerable:!0,configurable:!0});var s,t=new Map;s=function(e,i){for(var s=0;s<i.length-1;s+=2)t.set(i[s],{baseUrl:e,path:i[s+1]})}}),n("27Lyk").register(new URL("",import.meta.url).toString(),JSON.parse('["f9fpV","index.75826413.js","eyyUD","icons.0dd9027a.svg"]'));var r={};r=new URL("icons.0dd9027a.svg",import.meta.url).toString();var a={};function c(e,i,s,t,n){var r,a,c;let l=[2,3,5];if(!0===n)for(let i=3;i*i<=e;i+=2)e%i==0&&l.push(i);let o=0,u=e,p=i;for(;o<=l.length;)u%l[o]==0&&p%l[o]==0?(l[o],u/=l[o],p/=l[o]):o++;return r=p,a=u,c=s,1===r&&1===a?(c=`${t}${(parseInt(c)+1).toString()}`,`${c}`):0===a?`${t}${c}`:"0"==c?`${t}${a}/${r}`:`${t}${c} ${a}/${r}`}a=function(e){let i,s;if(e<0?(e=Math.abs(e),i="-"):i="",void 0===e)return"Your input was undefined.";if(isNaN(e))return`"${e}" is not a number.`;if(1e16==e)return`${i}9999999999999999`;if(e>1e16)return"Too many digits in your integer to maintain IEEE 754 Floating Point conversion accuracy.";if(Number.isInteger(e))return`${i}${e}`;if(e<1e-6)return"0";let t=e.toString(),n=t.split("."),r=n[0];if("0"==s&&"0"!==r)return r;if("0"==s&&"0"==r)return"0";if("99"==(s=t.length>=17?n[1].slice(0,n[1].length-1):n[1])&&"0"!==r)return`${r} 99/100`;if("99"==s&&"0"==r)return"99/100";if(1-parseFloat(`.${s}`)<.0011&&(s="999"),void 0==s)return r;let a=s.split("").reverse().join("").match(/^(\d+)\1{1,2}/);if(!a||!(s.length>2)){var l,o;return l=s,o=i,c(parseInt(l,10),Math.pow(10,l.length),r,o,!1)}{let e=a[0].split("").reverse().join(""),t=a[1].split("").reverse().join("");if(t.length>1){let e=t.split(""),i=1;for(let s=0;s<e.length;s++)i/=e[0]/e[s];1===i&&(t=e[0])}return t.length>1&&t.length%2==0&&(t=parseInt(t.slice(0,t.length/2),10)-parseInt(t.slice(t.length/2,t.length),10)==0?t.slice(0,t.length/2):t),function(e,i,s,t,n){let r=e.length-s.length>=1?e.length-s.length:1,a=Math.pow(10,r),l=parseFloat(`0.${e}`),o=Math.pow(10,i.length);return c(Math.round((l*o-l)*Math.pow(10,r)),(o-1)*a,t,n,!0)}(s,t,e,r,i)}};const l=document.querySelector(".recipe");!async function(){try{let i=await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"),{recipe:s}=(await i.json()).data;s={id:s.id,image:s.image_url,sourceUrl:s.source_url,ingredients:s.ingredients,publisher:s.publisher,title:s.title,servings:s.servings,cookingTime:s.cooking_time,ingredients:s.ingredients},console.log(s);let t=`
     <figure class="recipe__fig">
-      <img src="${n.image}" alt="${n.title}" class="recipe__img" />
+      <img src="${s.image}" alt="${s.title}" class="recipe__img" />
       <h1 class="recipe__title">
-        <span>${n.title}</span>
+        <span>${s.title}</span>
       </h1>
     </figure>
 
     <div class="recipe__details">
       <div class="recipe__info">
         <svg class="recipe__info-icon">
-          <use href="${/*@__PURE__*/e(c)}#icon-clock"></use>
+          <use href="${/*@__PURE__*/e(r)}#icon-clock"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--minutes">${n.cookingTime}</span>
+        <span class="recipe__info-data recipe__info-data--minutes">${s.cookingTime}</span>
         <span class="recipe__info-text">minutes</span>
       </div>
       <div class="recipe__info">
         <svg class="recipe__info-icon">
-          <use href="${/*@__PURE__*/e(c)}#icon-users"></use>
+          <use href="${/*@__PURE__*/e(r)}#icon-users"></use>
         </svg>
-        <span class="recipe__info-data recipe__info-data--people">${n.servings}</span>
+        <span class="recipe__info-data recipe__info-data--people">${s.servings}</span>
         <span class="recipe__info-text">servings</span>
 
         <div class="recipe__info-buttons">
           <button class="btn--tiny btn--increase-servings">
             <svg>
-              <use href="${/*@__PURE__*/e(c)}#icon-minus-circle"></use>
+              <use href="${/*@__PURE__*/e(r)}#icon-minus-circle"></use>
             </svg>
           </button>
           <button class="btn--tiny btn--increase-servings">
             <svg>
-              <use href="${/*@__PURE__*/e(c)}#icon-plus-circle"></use>
+              <use href="${/*@__PURE__*/e(r)}#icon-plus-circle"></use>
             </svg>
           </button>
         </div>
@@ -37,12 +37,12 @@ function e(e){return e&&e.__esModule?e.default:e}var t,n,r,i=globalThis,o={},s={
 
       <div class="recipe__user-generated">
         <svg>
-          <use href="${/*@__PURE__*/e(c)}#icon-user"></use>
+          <use href="${/*@__PURE__*/e(r)}#icon-user"></use>
         </svg>
       </div>
       <button class="btn--round">
         <svg class="">
-          <use href="${/*@__PURE__*/e(c)}#icon-bookmark-fill"></use>
+          <use href="${/*@__PURE__*/e(r)}#icon-bookmark-fill"></use>
         </svg>
       </button>
     </div>
@@ -50,15 +50,15 @@ function e(e){return e&&e.__esModule?e.default:e}var t,n,r,i=globalThis,o={},s={
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-      ${n.ingredients.map(t=>`
+      ${s.ingredients.map(i=>`
         <li class="recipe__ingredient">
           <svg class="recipe__icon">
-            <use href="${/*@__PURE__*/e(c)}#icon-check"></use>
+            <use href="${/*@__PURE__*/e(r)}#icon-check"></use>
           </svg>
-          <div class="recipe__quantity">${t.quantity?new r(t.quantity).toString():""}</div>
+          <div class="recipe__quantity">${i.quantity?a(i.quantity).toString():""}</div>
           <div class="recipe__description">
-            <span class="recipe__unit">${t.unit}</span>
-            ${t.description}
+            <span class="recipe__unit">${i.unit}</span>
+            ${i.description}
           </div>
         </li>
 
@@ -71,19 +71,19 @@ function e(e){return e&&e.__esModule?e.default:e}var t,n,r,i=globalThis,o={},s={
       <h2 class="heading--2">How to cook it</h2>
       <p class="recipe__directions-text">
         This recipe was carefully designed and tested by
-        <span class="recipe__publisher">${n.publisher}</span>. Please check out
+        <span class="recipe__publisher">${s.publisher}</span>. Please check out
         directions at their website.
       </p>
       <a
         class="btn--small recipe__btn"
-        href="${n.sourceUrl}"
+        href="${s.sourceUrl}"
         target="_blank"
       >
         <span>Directions</span>
         <svg class="search__icon">
-          <use href="${/*@__PURE__*/e(c)}#icon-arrow-right"></use>
+          <use href="${/*@__PURE__*/e(r)}#icon-arrow-right"></use>
         </svg>
       </a>
     </div>
-    `;u.innerHTML="",u.insertAdjacentHTML("afterbegin",i)}catch(e){alert(e)}}();
+    `;l.innerHTML="",l.insertAdjacentHTML("afterbegin",t)}catch(e){alert(e)}}();
 //# sourceMappingURL=index.75826413.js.map
--- index.8dda49e6.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".recipe");
-//# sourceMappingURL=index.8dda49e6.js.map
